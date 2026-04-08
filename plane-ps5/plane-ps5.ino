69498b8ebab7ee34f4da617a3f9d569b82470726
Your Name <you@example.com>
2026-04-08 16:37:57 +0500

    ps5 tr

--- plane-ps5/plane-ps5.ino
+++ plane-ps5/plane-ps5.ino
@@ -1,15 +1,125 @@
-#include <ps5Controller.h>
+#include <Bluepad32.h>
+
 #include <SPI.h>
 #include <LoRa.h>
 
 // определяем номера пинов, используемые трансивером
+#define SYNC_WORD 0xF1
+
 #define SS 5
 #define RST 14
 #define DIO0 2
 
-int ps5_r = 255;
-int ps5_g = 0;
-int ps5_b = 0;
+// start gamepad
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                           properties.product_id);
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
+    }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
+
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+            myControllers[i] = nullptr;
+            foundController = true;
+            break;
+        }
+    }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n",
+        ctl->index(),        // Controller Index
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+        ctl->gyroX(),        // Gyro X
+        ctl->gyroY(),        // Gyro Y
+        ctl->gyroZ(),        // Gyro Z
+        ctl->accelX(),       // Accelerometer X
+        ctl->accelY(),       // Accelerometer Y
+        ctl->accelZ()        // Accelerometer Z
+    );
+}
+
+void processGamepad(ControllerPtr ctl) {
+
+    // Another way to query controller data is by getting the buttons() function.
+    // See how the different "dump*" functions dump the Controller info.
+    // dumpGamepad(ctl);
+
+    ctl->setColorLED(255, 0, 0);
+
+//    ctl->axisX(),        // (-511 - 512) left X Axis
+//    ctl->axisY(),        // (-511 - 512) left Y axis
+//    ctl->axisRX(),       // (-511 - 512) right X axis
+//    ctl->axisRY(),       // (-511 - 512) right Y axis
+
+    String inputString = "<";
+    inputString += byteToHex(min(255, max(0, (int) ctl->axisRY() / 4 + 0x80)));
+    inputString += ",";
+    inputString += byteToHex(min(255, max(0, (int) ctl->axisRX() / 4 + 0x80)));
+    inputString += ",";
+    inputString += byteToHex(min(255, max(0, (int) ctl->axisY() / 2)));
+    inputString += ">";
+    
+    Serial.print("Sending - ");
+    Serial.println(inputString);
+    
+    //Отправляем сообщение
+    LoRa.beginPacket();
+    LoRa.print(inputString);
+    LoRa.endPacket();
+  
+}
+
+void processControllers() {
+    for (auto myController : myControllers) {
+        if (myController && myController->isConnected() && myController->hasData()) {
+            if (myController->isGamepad()) {
+                processGamepad(myController);
+                break;
+            } else {
+                Serial.println("Unsupported controller");
+            }
+        }
+    }
+}
+
+
+// end gamepad
 
 String byteToHex(byte b) {
   char hex[3];
@@ -18,10 +128,7 @@ String byteToHex(byte b) {
 }
 
 void setup() {
-  // ps5 setup
   Serial.begin(115200);
-  ps5.begin("48:18:8D:D7:02:C3"); //replace with MAC address of your controller
-  Serial.println("Ps5 Ready.");
 
   // Lora setup
   Serial.println("LoRa Sender");
@@ -30,71 +137,44 @@ void setup() {
   LoRa.setPins(SS, RST, DIO0);
   
   //замените LoRa.begin(---E-) частотой, которую вы собираетесь использовать 
-  while (!LoRa.begin(433E6)) {
+  while (!LoRa.begin(915E6)) {
     Serial.println(".");
     delay(500);
   }
 
   LoRa.setTxPower(17);
-  LoRa.setSpreadingFactor(9);
-  LoRa.setSignalBandwidth(125E3);
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(126E3);
   LoRa.setCodingRate4(8);
-  LoRa.setSyncWord(0xF1);
+  LoRa.setSyncWord(SYNC_WORD);
   
   Serial.println("LoRa Initializing OK!");
+
+  // gamepad setup 
+
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
   
 }
 
 void loop() {
-  while (ps5.isConnected() == false) { // commented out as ps5 controller seems to connect quicker when microcontroller is doing nothing
-    Serial.println("PS5 controller not found.");
-    delay(300);
-  }
 
-  while (ps5.isConnected() == true) {
+  bool dataUpdated = BP32.update();
+  processControllers();
 
-//    if (ps5.L1()) Serial.println("L1 Button");
-//    if (ps5.R1()) Serial.println("R1 Button");
-//
-//    if (ps5.Share()) Serial.println("Share Button");
-//    if (ps5.Options()) Serial.println("Options Button");
-//    if (ps5.L3()) Serial.println("L3 Button");
-//    if (ps5.R3()) Serial.println("R3 Button");
-//
-//    if (ps5.PSButton()) Serial.println("PS Button");
-//    if (ps5.Touchpad()) Serial.println("Touch Pad Button");
-//
-//    if (ps5.L2()) {
-//      Serial.printf("L2 button at %d\n", ps5.L2Value());
-//    }
-//    if (ps5.R2()) {
-//      Serial.printf("R2 button at %d\n", ps5.R2Value());
-//    }
-//
-//    Serial.printf("Left Stick x at %d\n", ps5.LStickX());
-//    Serial.printf("Left Stick y at %d\n", ps5.LStickY());
-//    Serial.printf("Right Stick x at %d\n", ps5.RStickX());
-//    Serial.printf("Right Stick y at %d\n", ps5.RStickY());
-    
-//    String inputString = "<80,80,00>";
-    String inputString = "<";
-    inputString += byteToHex(min(255, max(0, (int) ps5.RStickY() + 0x80)));
-    inputString += ",";
-    inputString += byteToHex(min(255, max(0, (int) ps5.RStickX() + 0x80)));
-    inputString += ",";
-    inputString += byteToHex(min(255, max(0, (int) ps5.LStickY() * 2)));
-    inputString += ">";
-    
-    Serial.print("Sending - ");
-    Serial.println(inputString);
-   
-    //Отправляем сообщение
-    LoRa.beginPacket();
-    LoRa.print(inputString);
-    LoRa.endPacket();
-
-    // This delay is to make the output more human readable
-    // Remove it when you're not trying to see the output
-    delay(50);
-  }
+  delay(50);
+  
 }
